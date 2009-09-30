a401e76e655ee6db50cbd2eb494903e2445672dd
emolinero <emolinero@d4141b02-adb7-11de-83b6-dd1e45f03187>
2009-09-30 20:10:10 +0000

    Metodo Calcular Subtotal

--- codigo/src/ar/edu/uade/ioo/grupo5/tpo/bo/Comanda.java
+++ codigo/src/ar/edu/uade/ioo/grupo5/tpo/bo/Comanda.java
@@ -1,5 +1,6 @@
 package ar.edu.uade.ioo.grupo5.tpo.bo;
 
+import java.util.Iterator;
 import java.util.Vector;
 
 /**
@@ -7,25 +8,31 @@ Project : TP_IPOO_1
 File Name : Comanda.java
 Date : 30/09/2009
 @author Grupo 5 
-
 */
-
-
-
-
 
 public class Comanda {
 	private Vector<ItemComanda> items;
+	//TODO estos atributos van o no van?!?!
+	private Mesa mesa;
+	private Mozo mozo;
+	
+	
+	public void Comanda(Mesa mesa, Mozo mozo) {
+		this.mesa = mesa;
+		this.mozo = mozo;
+	}
+	
 	public double calcularTotal() {
-		double ret=0;
-		return ret;
+		double total=0;
+		for(int i=0;i<items.size();i++){
+			total += items.elementAt(i).calcularSubtotal();
+		}
+		return total;
 	}
 	
 	public void addItem(ItemComanda item) {
-	
+		items.add(item);
 	}
 	
-	public void Comanda(Mesa mesa, Mozo mozo) {
 	
-	}
 }
