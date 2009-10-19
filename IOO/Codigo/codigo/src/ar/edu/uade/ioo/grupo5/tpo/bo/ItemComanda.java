bf298ab372f0a335ff4cde5dc69cc2c6d94a85ee
emmanuelt88 <emmanuelt88@d4141b02-adb7-11de-83b6-dd1e45f03187>
2009-10-19 22:18:09 +0000

    Se movieron las carpetas de la materia a IOO

---
+++ IOO/Codigo/codigo/src/ar/edu/uade/ioo/grupo5/tpo/bo/ItemComanda.java
@@ -0,0 +1,49 @@
+package ar.edu.uade.ioo.grupo5.tpo.bo;
+
+import java.util.Vector;
+
+/**
+Project : TP_IPOO_1
+File Name : ItemComanda.java
+Date : 30/09/2009
+@author Grupo 5 
+
+*/
+
+
+
+
+
+public class ItemComanda {
+	private Consumible consumible;
+	private int cantidad;
+	
+	public ItemComanda(Consumible consumible, int cantidad) {
+		this.consumible = consumible;
+		this.cantidad = cantidad;
+		
+		Vector<ItemProducto> itemsProductos = this.consumible.getItemsProducto();
+		
+		
+		for (ItemProducto itemProducto: itemsProductos) {
+			double cantidadProducto = itemProducto.getCantidad();
+			
+			Producto producto = itemProducto.getProducto();
+			
+			producto.descontarStock(cantidadProducto * cantidad);
+			
+			
+		}
+		 
+	}
+	
+	public Consumible getConsumible() {
+		return this.consumible;
+	}
+	
+	public double calcularSubtotal() {
+		return consumible.getPrecio() * cantidad;
+	}
+	
+	
+}
