eadac4ad2c3f143c122a0678651743f127074791
emmanuelt88 <emmanuelt88@d4141b02-adb7-11de-83b6-dd1e45f03187>
2009-11-15 19:39:16 +0000

    Agrego las excepciones

---
+++ trunk/IOO/Codigo/codigo/src/ar/edu/uade/ioo/grupo5/tpo/common/NotificationException.java
@@ -0,0 +1,7 @@
+package ar.edu.uade.ioo.grupo5.tpo.common;
+
+public class NotificationException extends Exception{
+	public NotificationException(String mensaje) {
+		super(mensaje);
+	}
+}
---
+++ trunk/IOO/Codigo/codigo/src/ar/edu/uade/ioo/grupo5/tpo/common/ValidationException.java
@@ -0,0 +1,7 @@
+package ar.edu.uade.ioo.grupo5.tpo.common;
+
+public class ValidationException extends Exception{
+	public ValidationException(String mensaje) {
+		super(mensaje);
+	}
+}
