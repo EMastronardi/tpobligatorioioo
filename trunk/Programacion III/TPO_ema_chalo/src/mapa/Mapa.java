1abf7730e540d5972def89bcd2248bf0f472eb67
emastronardi <emastronardi@d4141b02-adb7-11de-83b6-dd1e45f03187>
2010-06-27 05:00:12 +0000

    Agregado de metodos varios

--- trunk/Programacion III/TPO_ema_chalo/src/mapa/Mapa.java
+++ trunk/Programacion III/TPO_ema_chalo/src/mapa/Mapa.java
@@ -1,15 +1,28 @@
 package mapa;
 
+import grafico.Punto;
 import grafico.PuntoTDA;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Mapa extends MapaBase{
 
 	@Override
 	public List<PuntoTDA> getAdyacentes(PuntoTDA ubicacion) {
-		// TODO Auto-generated method stub
-		return null;
+		List<PuntoTDA> lista = new ArrayList<PuntoTDA>();
+		int x = ubicacion.getX();
+		int y = ubicacion.getY();
+		if (getDensidad(x-1, y-1)<3)lista.add(new Punto(x-1, y-1));
+		if (getDensidad(x-1, y)<3)lista.add(new Punto(x-1, y));
+		if (getDensidad(x-1, y+1)<3)lista.add(new Punto(x-1, y+1));
+		if (getDensidad(x, y-1)<3)lista.add(new Punto(x, y-1));
+		if (getDensidad(x, y+1)<3)lista.add(new Punto(x, y+1));
+		if (getDensidad(x+1, y-1)<3)lista.add(new Punto(x+1, y-1));
+		if (getDensidad(x+1, y)<3)lista.add(new Punto(x+1, y));
+		if (getDensidad(x+1, y+1)<3)lista.add(new Punto(x+1, y+1));
+			
+		return lista;
 	}
 
 }
